9ddfdee52c1dc352d5c98cdfe38ffbd32126557d
APikielny <apikielny@gmail.com>
2015-09-15 14:37:28 -0400

    Updated from Brython Server: September 15, 2015 2:37:27 PM EDT

--- birthday.py
+++ birthday.py
@@ -36,14 +36,33 @@ from calendar import month_name
 todaymonth = datetime.today().month
 todaydate = datetime.today().day
 monthname = month_name[todaymonth]
+print (monthname)
+print (todaydate)
 
 name=input("Hello, what is your name? ")
 month=input("Hi " + name + ", what was the name of the month you were born in? ")
-year=input("And what year were you born in, " + name + "? ")
+year=int(input("And what year were you born in, " + name + "? "))
 day=input("And the day? ")
+if (monthname=="December" or monthname=="January" or monthname=="February"):
+    season="winter"
+elif (monthname=="March" or monthname=="April" or monthname=="May"):
+    season="spring"
+elif (monthname=="June" or monthname=="July" or monthname=="August"):
+    season="summer"
+else:
+    season="fall"
+if (year>=2000):
+    yeartype="two thousands"
+elif (year>=1990):
+    yeartype="nineties"
+elif (year>=1980):
+    yeartype="eighties"
+else:
+    yeartype="stone age"
+
 if (month=="October" and day=="31"):
     print("You were born on Halloween!")
-elif (month==monthname and day==todaydate):
+elif (month == monthname and int(day) == todaydate):
     print("Happy birthday!")
-else:
-    print(name + ", you are a winter baby of the nineties.")
+
+print (name+", you are a "+season+" baby of the " + yeartype +".")
